7db2c71f69635b2be5eb8a76dc47d790f5ed751c
saharabanu <saharabanungn@gmail.com>
2021-10-27 17:10:36 +0600

    some api added

--- index.js
+++ index.js
@@ -67,6 +67,9 @@ run().catch(console.dir);
 app.get('/',(req,res)=>{
     res.send('Running Genius Server');
 })
+app.get('/hello',(req,res)=>{
+    res.send('Running Hello Upadated here');
+})
 app.listen(port,()=>{
     console.log('Running Genius Server',port)
 })
